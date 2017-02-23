fdfe9692fc10e630178647f267fffcd0abf85fcd
Chris Wainner and Dallas Slaughter <chris@>
2017-02-23 15:37:30 -0800

    Add js logic to read arrays by crypto rules, output encrypted string

--- js/scripts.js
+++ js/scripts.js
@@ -3,42 +3,57 @@ var encryptFunction = function(unencrypted) {
   var index = 0;
   var removeSpecials = unencrypted.toLowerCase().match(/[a-zA-Z]/gi)
   var numChars = removeSpecials.length;
-  var dimensions = gridDimensions(numChars);
-  var xAxis = dimensions[0];
-  var yAxis = dimensions[1];
+  var xAxis = gridDimensions(numChars);
   var result = [];
-  // [dimensions[0]][dimensions[1]];
 
-
-  for(var x = 0; x < xAxis; x++){
-
-    result.push(removeSpecials.slice(index, index+yAxis));
-    index += yAxis;
-    console.log(index);
-    // for(var y = 0; y < dimensions[1]; y++){
-    //   if (index <= numChars) {
-    //   var tempArray = [];
-    //   tempArray.push(removeSpecials[index]);
-    //   result.push(tempArray);
-    //   index++;
-    // } else if(index >= numChars && index < (dimensions[0]*dimensions[1])){
-    //   result.push("");
-    // }
-    // }
+  for(var x = 0; x < removeSpecials.length; x+=xAxis){
+    result.push(removeSpecials.slice(x, x+xAxis));
   }
-  return result;
+
+  var output = cryptoOutput(result);
+  output = chunk(output, 5).join(' ');
+  return output;
 }
+
+var cryptoOutput = function(array) {
+  var outputString = "";
+  
+  for (var x = 0; x < array.length; x++) {
+    array.forEach(function(subArray) {
+      var arrayToString = subArray.join("");
+
+      if (arrayToString[x] === undefined) {
+        outputString += ""
+      } else {
+        outputString = outputString + arrayToString[x];
+      }
+
+    });
+  }
+  return outputString;
+}
+
+function chunk(stringLength, n) {
+  var result = [];
+  var i;
+  var len;
+
+  for(i = 0, len = stringLength.length; i < len; i += n) {
+    result.push(stringLength.substr(i, n))
+  }
+
+  return result
+};
 
 var gridDimensions = function(length) {
   var sqrCheck = Math.sqrt(length);
   var xSide = parseInt(Math.sqrt(length));
-  var ySide = parseInt(length/xSide);
-  var dimensions = [xSide, ySide];
 
   if (sqrCheck % 1 !== 0) {
-    xSide = xSide + 1;
+    xSide += 1;
   }
 
+  var dimensions = xSide;
   return dimensions;
 }
 
@@ -48,6 +63,5 @@ $(document).ready(function(){
     var value = $("#inputString").val();
     $("#result").show();
     $("#resultP").text(encryptFunction(value));
-    console.log(encryptFunction(value));
   });
 });
